b897e83137103f7aace545df9792b596a12741c6
Muhammad Asad V P <mhdasadvp@gmail.com>
2024-07-31 20:20:09 +0530

    Optional Assignment Uploaded

---
+++ Optional_Assignment-1/demo/src/main/java/com/example/demo/MathController.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+
+    @GetMapping("/sum")
+    public int sum(@RequestParam int a, @RequestParam int b) {
+        return a + b;
+    }
+
+    @GetMapping("/subtract")
+    public int subtract(@RequestParam int a, @RequestParam int b) {
+        return a - b;
+    }
+
+    @GetMapping("/multiply")
+    public int multiply(@RequestParam int a, @RequestParam int b) {
+        return a * b;
+    }
+
+    @GetMapping("/divide")
+    public double divide(@RequestParam int a, @RequestParam int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed");
+        }
+        return (double) a / b;
+    }
+}
